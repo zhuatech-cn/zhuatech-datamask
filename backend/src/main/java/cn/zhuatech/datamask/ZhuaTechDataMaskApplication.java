07dece1552a8a577d9fba9f12fafc136db039377
nero <233401861@qq.com>
2026-08-22 10:31:20 +0800

    chore: add website to copyright headers

--- backend/src/main/java/cn/zhuatech/datamask/ZhuaTechDataMaskApplication.java
+++ backend/src/main/java/cn/zhuatech/datamask/ZhuaTechDataMaskApplication.java
@@ -1,2 +1,2 @@
-/* Copyright 2026 上海如静知华信息科技有限公司 */
+/* Copyright 2026 上海如静知华信息科技有限公司 · https://www.zhuatech.cn/ */
 package cn.zhuatech.datamask;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplication public class ZhuaTechDataMaskApplication{public static void main(String[]a){SpringApplication.run(ZhuaTechDataMaskApplication.class,a);}}
